d6c15e1b1dc8183065d615f3da04f5df5a6df108
Simon Vitriago <cypictronic@gmail.com>
2024-07-11 22:29:24 -0400

    Añadiendo Archivos

---
+++ app3.js
@@ -0,0 +1,54 @@
+/*
+1. Crea una función que calcule el índice de masa corporal (IMC) 
+de una persona a partir de su altura en metros y peso en kilogramos, 
+que se recibirán como parámetros.
+*/
+
+//Peso en kilogramos [kg]
+//Altura en metros [m]
+
+function IMC(altura, peso)
+{
+    return peso/Math.pow(altura,2); 
+}
+
+function calcular()
+{
+    let altura = parseFloat(document.getElementById('inpAltura').value);
+    let peso = parseFloat(document.getElementById('inpPeso').value);
+
+    let indiceMasaMuscular = IMC(altura,peso);
+    let condicion = "";    
+
+    if(indiceMasaMuscular>35)
+        {
+            condicion = "Obesidad Morbida";
+        }
+        else if(indiceMasaMuscular>30 && indiceMasaMuscular<34.90)
+        {
+            condicion = "Obesidad";
+        }
+        else if(indiceMasaMuscular>25 && indiceMasaMuscular<29.90)
+            {
+                condicion = "Sobrepeso";
+            }
+            else if(indiceMasaMuscular>18.50 && indiceMasaMuscular<24.90)
+                {
+                    condicion = "Normopeso";
+                }    
+                else
+                {
+                    condicion = "Peso insuficiente";
+                }
+        
+                                
+        console.log(`Usted posee un IMC de ${Number.parseFloat(indiceMasaMuscular).toFixed(2)} y estado nutricional con ${condicion}`);
+        console.log("altura: " + altura);
+        console.log("peso: " + peso);
+        console.log("IMC: " + Number.parseFloat(indiceMasaMuscular).toFixed(2));
+        
+        document.querySelector('#resultado').innerHTML = `Usted posee un IMC de ${Number.parseFloat(indiceMasaMuscular).toFixed(2)} y estado nutricional con ${condicion}`;
+}
+
+//let altura = parseFloat(prompt("Introduzca su altura [m]"));
+//let peso =  parseFloat(prompt("Introduzca su peso [kg]"));
