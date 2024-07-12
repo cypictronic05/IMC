aef6cdaf3a35593f7358c598e733eadc7c9edc09
Simon Vitriago <cypictronic@gmail.com>
2024-07-11 22:41:36 -0400

    Validando posible división por cero

--- app3.js
+++ app3.js
@@ -17,38 +17,45 @@ function calcular()
     let altura = parseFloat(document.getElementById('inpAltura').value);
     let peso = parseFloat(document.getElementById('inpPeso').value);
 
-    let indiceMasaMuscular = IMC(altura,peso);
-    let condicion = "";    
+    if(peso!=0)
+    {
+        let indiceMasaMuscular = IMC(altura,peso);
+        let condicion = "";
 
-    if(indiceMasaMuscular>35)
-        {
-            condicion = "Obesidad Morbida";
-        }
-        else if(indiceMasaMuscular>30 && indiceMasaMuscular<34.90)
-        {
-            condicion = "Obesidad";
-        }
-        else if(indiceMasaMuscular>25 && indiceMasaMuscular<29.90)
+        if(indiceMasaMuscular>35)
             {
-                condicion = "Sobrepeso";
+                condicion = "Obesidad Morbida";
             }
-            else if(indiceMasaMuscular>18.50 && indiceMasaMuscular<24.90)
+            else if(indiceMasaMuscular>30 && indiceMasaMuscular<34.90)
+            {
+                condicion = "Obesidad";
+            }
+            else if(indiceMasaMuscular>25 && indiceMasaMuscular<29.90)
                 {
-                    condicion = "Normopeso";
-                }    
-                else
-                {
-                    condicion = "Peso insuficiente";
+                    condicion = "Sobrepeso";
                 }
+                else if(indiceMasaMuscular>18.50 && indiceMasaMuscular<24.90)
+                    {
+                        condicion = "Normopeso";
+                    }    
+                    else//IMC<=18.50
+                    {
+                        condicion = "Peso insuficiente";
+                    }        
+
+                    document.querySelector('#resultado').innerHTML = `Usted posee un IMC de ${Number.parseFloat(indiceMasaMuscular).toFixed(2)} y estado nutricional con ${condicion}`;
+    }else
+    {
+        alert('Peso debe ser distinto a cero');
+    }        
         
-                                
-        console.log(`Usted posee un IMC de ${Number.parseFloat(indiceMasaMuscular).toFixed(2)} y estado nutricional con ${condicion}`);
-        console.log("altura: " + altura);
-        console.log("peso: " + peso);
-        console.log("IMC: " + Number.parseFloat(indiceMasaMuscular).toFixed(2));
-        
-        document.querySelector('#resultado').innerHTML = `Usted posee un IMC de ${Number.parseFloat(indiceMasaMuscular).toFixed(2)} y estado nutricional con ${condicion}`;
 }
+
+// console.log(`Usted posee un IMC de ${Number.parseFloat(indiceMasaMuscular).toFixed(2)} y estado nutricional con ${condicion}`);
+// console.log("altura: " + altura);
+// console.log("peso: " + peso);
+// console.log("IMC: " + Number.parseFloat(indiceMasaMuscular).toFixed(2));
+
 
 //let altura = parseFloat(prompt("Introduzca su altura [m]"));
 //let peso =  parseFloat(prompt("Introduzca su peso [kg]"));
